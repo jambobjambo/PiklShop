ca4b6d0453560825cbb1db12097dc575acb5e1b6
jambobjambo <jamie.a.mcinally@gmail.com>
2016-11-07 21:02:00 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -2,12 +2,36 @@
  * Created by jamie on 07/11/2016.
  */
 var express  = require('express')
+var request = require('request');
 
-var app = express();
-
-app.get('/', function (req, res) {
-    res.send('This is PIKL Server');
+var vision = require('@google-cloud/vision')({
+    projectId: 'piklshop-42f40',
+    keyFilename: 'keyfile.json'
 });
+var app = express();
+var img = 'http://smashinghub.com/wp-content/uploads/2012/02/Girl-in-red-dress-22.jpg';
+
+app.get('/imagesearch', function (req, res) {
+    var labelList = [];
+    vision.detectLabels(img, function(err, labels, apiResponse) {
+        labelList.push(labels[0])
+        labelList.push(labels[1])
+        labelList.push(labels[2])
+        res.send(labelList)
+    });
+});
+
+app.get('/productsearch', function(req,res){
+    request('http://api.shopstyle.com/api/v2/products?pid=uid625-36772825-65&fts=dress+clothing+red&offset=0&limit=10', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var cleaned = body.trim();
+            var clothelist = JSON.parse(cleaned);
+            var jsonApp = "{ 'Products' : [{ '1' :{ 'name' : '" + clothelist.products[0].brandedName + "', 'image' : '" + clothelist.products[0].image.sizes.Best.url + "', 'price' : '" + clothelist.products[0].priceLabel + "'}}]}"
+            res.send(jsonApp);
+        }
+    })
+});
+
 
 var server = app.listen(process.env.PORT || '8080', function () {
     console.log('App listening on port %s', server.address().port);
